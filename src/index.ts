1e4d1e6b3475468a435f1706029a9bf7cd76a97e
Peter K. Moss <peter@moss.dk>
2022-11-17 20:44:00 +0100

    add routes for remaining controllers + handle not found routes

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,19 @@ import * as cors from 'cors'
 
 import DieRouter from './routes/Die.route'
 import TraditionRouter from './routes/Tradition.route'
+import ClassRouter from './routes/CharacterClass.route'
+import CharacterRouter from './routes/Character.route'
+import SphereRouter from './routes/Sphere.route'
+import ItemRouter from './routes/Item.route'
+import { connect } from './util/mongo'
+
+const testMongoConnection = async () => {
+  await connect()
+}
 
 async function init() {
+  await testMongoConnection()
+
   const port = 3000
   const app = express()
 
@@ -16,6 +27,14 @@ async function init() {
 
   app.get('/die', DieRouter)
   app.use('/tradition', TraditionRouter)
+  app.use('/class', ClassRouter)
+  app.use('/item', ItemRouter)
+  app.use('/character', CharacterRouter)
+  app.use('/sphere', SphereRouter)
+
+  app.use((req, res, next) => {
+    res.status(404).send({ result: null, error: 'resource not found' })
+  })
 
   app.use((err, req, res, next) => {
     console.error(err.message)
